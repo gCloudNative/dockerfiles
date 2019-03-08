084479c7cd094a1afab4c0d2899a3970a8e22ad5
Yang Sen <nuaays@gmail.com>
2019-03-08 11:18:59 +0800

    update

--- tomcat_8.0.51-jdk1.8.0_202/build.sh
+++ tomcat_8.0.51-jdk1.8.0_202/build.sh
@@ -1,5 +1,5 @@
 
 
-docker build --pull -t nuaays/tomcat:8.0.51-jdk1.8.0_202 .
+docker build -t nuaays/tomcat:8.0.51-jdk1.8.0_202 .
 docker push nuaays/tomcat:8.0.51-jdk1.8.0_202
 echo "nuaays/tomcat:8.0.51-jdk1.8.0_202"
---
+++ tomcat_8.0.53-jdk1.8.0_202/build.sh
@@ -0,0 +1,5 @@
+
+
+docker build -t nuaays/tomcat:8.0.53-jdk1.8.0_202 .
+#docker push nuaays/tomcat:8.0.53-jdk1.8.0_202
+echo "nuaays/tomcat:8.0.53-jdk1.8.0_202"
