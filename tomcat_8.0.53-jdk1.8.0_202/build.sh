23b604c17cadba202ca530eff1b111b6c5c4b57d
Yang Sen <nuaays@gmail.com>
2019-03-08 13:49:56 +0800

    update

--- tomcat_8.0.53-jdk1.8.0_202/build.sh
+++ tomcat_8.0.53-jdk1.8.0_202/build.sh
@@ -3,3 +3,7 @@
 docker build -t nuaays/tomcat:8.0.53-jdk1.8.0_202 .
 #docker push nuaays/tomcat:8.0.53-jdk1.8.0_202
 echo "nuaays/tomcat:8.0.53-jdk1.8.0_202"
+docker push nuaays/tomcat:8.0.53-jdk1.8.0_202
+
+
+
